0768b4a3b9c768a083158ceb1d6693e5a70de642
Ramon Pereira <ramon.alves@live.com>
2019-03-16 14:18:39 -0300

    Estrutura inicial

---
+++ src/main/java/com/ramon/pereira/albumstore/AlbumstoreApplication.java
@@ -0,0 +1,13 @@
+package com.ramon.pereira.albumstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbumstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlbumstoreApplication.class, args);
+	}
+
+}
