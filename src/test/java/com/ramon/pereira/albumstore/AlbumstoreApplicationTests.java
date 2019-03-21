7390370940c7a9050977907162402955539d6015
Ramon Pereira <ramon.pereira@luizalabs.com>
2019-03-20 21:52:27 -0300

    limpando CI

--- src/test/java/com/ramon/pereira/albumstore/AlbumstoreApplicationTests.java
+++ src/test/java/com/ramon/pereira/albumstore/AlbumstoreApplicationTests.java
@@ -1,9 +1,7 @@
 package com.ramon.pereira.albumstore;
 
-import com.ramon.pereira.albumstore.services.SpotifyService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -13,6 +11,6 @@ public class AlbumstoreApplicationTests {
 
   @Test
   public void main() {
-    AlbumstoreApplication.main(new String[] {"--spring.config.location=classpath:application-test.yml"});
+   AlbumstoreApplication.main(new String[] {"--spring.config.location=classpath:application-test.yml"});
   }
 }
