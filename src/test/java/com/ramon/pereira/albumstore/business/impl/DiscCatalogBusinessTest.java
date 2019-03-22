4b2f9dcdc7e70d42aefe20bd74d11a67ee370d4f
Ramon Pereira <ramon.alves@live.com>
2019-03-21 23:10:44 -0300

    testes do recurso DiscCatalogBusiness

--- src/test/java/com/ramon/pereira/albumstore/business/impl/DiscCatalogBusinessTest.java
+++ src/test/java/com/ramon/pereira/albumstore/business/impl/DiscCatalogBusinessTest.java
@@ -4,6 +4,7 @@ import com.github.javafaker.Faker;
 import com.ramon.pereira.albumstore.business.DiscCatalogBusiness;
 import com.ramon.pereira.albumstore.config.SpringTestConfiguration;
 import com.ramon.pereira.albumstore.exception.DiscNotFoundException;
+import com.ramon.pereira.albumstore.model.enDiscGenre;
 import com.ramon.pereira.albumstore.repository.DiscCatalogRepository;
 import com.ramon.pereira.albumstore.seed.DiscSeeder;
 import com.ramon.pereira.albumstore.services.SpotifyService;
@@ -14,10 +15,13 @@ import org.mockito.ArgumentMatchers;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Arrays;
 import java.util.Optional;
 
 @TestPropertySource(properties = {"spring.config.location=classpath:application-test.yml"})
@@ -55,6 +59,28 @@ public class DiscCatalogBusinessTest {
                 .thenThrow(new DiscNotFoundException());
 
         this.discCatalogBusiness.findById(1);
+    }
+
+    @Test
+    public void findByGenreOrderByNameAscSearchResults() {
+        Mockito.when(this.discCatalogRepository.findByGenreOrderByNameAsc(ArgumentMatchers.any(enDiscGenre.class),ArgumentMatchers.any(Pageable.class)))
+                .thenReturn(Optional.of(Arrays.asList(DiscSeeder.disc(1),DiscSeeder.disc(2))));
+
+        var disks = discCatalogRepository.findByGenreOrderByNameAsc(enDiscGenre.ROCK, PageRequest.of(1, 10));
+
+        Assert.assertTrue(disks.isPresent());
+        Assert.assertTrue(disks.get().size() > 0);
+    }
+
+    @Test
+    public void findByGenreOrderByNameAscEmptyResult() {
+        Mockito.when(this.discCatalogRepository.findByGenreOrderByNameAsc(ArgumentMatchers.any(enDiscGenre.class),ArgumentMatchers.any(Pageable.class)))
+                .thenReturn(Optional.of(Arrays.asList()));
+
+        var disks = discCatalogRepository.findByGenreOrderByNameAsc(enDiscGenre.POP, PageRequest.of(1, 10));
+
+        Assert.assertTrue(disks.isPresent());
+        Assert.assertEquals(0, disks.get().size());
     }
 
 }
